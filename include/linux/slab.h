a3fe641df134f5fcc97e721b5ceffb4d4e3f4c33
Yuhong Zhong <nanua.zqz@gmail.com>
2021-01-11 19:07:39 -0500

    minor fix

--- include/linux/slab.h
+++ include/linux/slab.h
@@ -138,8 +138,8 @@
 #define _IMPOSTER_ARR_SIZE 3750000
 extern long *_imposter_sub;
 extern long *_imposter_comp;
-extern atomic_t _imposter_sub_index;
-extern atomic_t _imposter_comp_index;
+extern atomic_long_t _imposter_sub_index;
+extern atomic_long_t _imposter_comp_index;
 
 struct mem_cgroup;
 /*
