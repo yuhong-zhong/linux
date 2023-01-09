5aa7fbed90cd7e44560dc3505a0d96aaa6513bcc
Yuhong Zhong <yz@cs.columbia.edu>
2023-01-09 08:37:51 -0800

    Disable PMI throttling

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -8890,7 +8890,7 @@ int perf_event_overflow(struct perf_event *event,
 			  struct perf_sample_data *data,
 			  struct pt_regs *regs)
 {
-	return __perf_event_overflow(event, 1, data, regs);
+	return __perf_event_overflow(event, 0, data, regs);
 }
 
 /*
